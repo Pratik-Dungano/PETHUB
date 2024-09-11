b4f597aea35163b6389b9bc7f210ac8b915252b5
Pratik <pratikpemendra@gmail.com>
2024-09-11 14:03:35 +0530

    updated package-lock-json

--- Pet-Adoption-Platform-main/src/components/aboutus/AboutUs.jsx
+++ Pet-Adoption-Platform-main/src/components/aboutus/AboutUs.jsx
@@ -1,111 +1,58 @@
+import { BsFillBarChartFill, BsFillBriefcaseFill } from 'react-icons/bs';
+import { BiAtom } from 'react-icons/bi';
 
-import contact from '../../assets/contact.jpg'
-import  { useRef } from 'react';
-import emailjs from '@emailjs/browser';
-import { ToastContainer, toast } from 'react-toastify';
-  import 'react-toastify/dist/ReactToastify.css';
+import contact from '../../../../assets/pets.jpg';
+import aboutus from '../../../../assets/aboutus.jpg';
 
-import AOS from 'aos';
-import 'aos/dist/aos.css';
-import { useEffect } from 'react';
+const AboutUs = () => {
+  return (
+    <div className="relative mt-20" style={{ backgroundImage: url(${contact}), backgroundSize: 'cover', backgroundAttachment: 'fixed' }}>
+      <div className="absolute inset-0 bg-black opacity-70" />
 
-
-  
-const Contact = () => {
-  useEffect(() => {
-    AOS.init({ duration: '1000' });
-  }, []);
-
-  const form = useRef();
-
-  const sendEmail = (e) => {
-    e.preventDefault();
-
-    emailjs.sendForm('service_9q2dnan', 'template_4g8zlzm', form.current, 'eFsOrcFwb7oeD_oO1')
-      .then((result) => {
-          console.log(result.text);
-          toast.success('Send Email Successfully', {
-            position: toast.POSITION.RIGHT_TOP,
-            autoClose: 6000,
-      hideProgressBar: false,
-      closeOnClick: true,
-      pauseOnHover: true,
-      draggable: true,
-      progress: undefined,
-      theme: "colored", 
-          });
-      }, (error) => {
-          console.log(error.text);
-      });
-  };
-    return (
-        <div className='section h-screen mt-20 lg:mb-20 mb-96 ' id='contact'>
-            <div className='flex flex-col'><h1 className='text-4xl font-bold text-center  my-20 gradient-text'>GET IN TOUCH</h1></div>
-          <div className='flex flex-col lg:flex-row gap-10 items-center justify-center'>
-            <div>
-<img src={contact} className='w-96 header' alt="" data-aos="zoom-in-up" />
+      <div className="relative z-10 text-white py-16">
+        <div className="flex flex-col items-center justify-center mt-20 mb-20" id="services">
+          <h3 className="text-[#ff0000]">Know about us</h3>
+          <h1 className="text-white text-2xl md:text-4xl font-metamorphous font-bold text-center">_About PetHub_</h1>
+        </div>
+        <div className="flex items-center justify-center">
+          <div className="px-10 py-10 w-full lg:max-w-[1200px] mx-auto">
+            <div className="flex gap-16 items-center justify-center">
+              <div>
+                <BsFillBarChartFill className="text-3xl text-warning" />
+              </div>
+              <div>
+                <h1 className="text-2xl font-bold">Our Vision</h1>
+                <p>PetHub envisions a world where every pet finds their forever home. We're committed to revolutionizing pet adoption by connecting loving pet owners with deserving animals.</p>
+              </div>
             </div>
-            <div className="relative flex flex-col text-gray-700 bg-white shadow-md w-96 rounded-xl bg-clip-border">
-  <div data-aos='fade-up'
-    className="relative grid mx-4 mb-4 -mt-6 overflow-hidden text-white shadow-lg h-28 place-items-center rounded-xl  btn-bgg bg-clip-border shadow-gray-900/20">
-    <h3 className="block font-sans text-3xl antialiased font-semibold leading-snug tracking-normal  text-white">
-     Send Message
-    </h3>
-  </div>
-  <form ref={form} onSubmit={sendEmail}>
-  <div className="flex flex-col gap-4 p-6">
-    <div className="relative h-11 w-full min-w-[200px]">
-      <input
-        className="w-full h-full px-3 py-3 font-sans text-sm font-normal transition-all bg-transparent border rounded-md peer border-blue-gray-200 border-t-transparent text-blue-gray-700 outline outline-0 placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-gray-900 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
-        placeholder=" " type='email' name='from_email' required />
-      <label
-        className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none !overflow-visible truncate text-[11px] font-normal leading-tight text-gray-500 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.1] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-gray-900 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:!border-gray-900 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:!border-gray-900 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
-        Email
-      </label>
-    </div>
-    <div className="relative h-11 w-full min-w-[200px]">
-      <input
-        className="w-full h-full px-3 py-3 font-sans text-sm font-normal transition-all bg-transparent border rounded-md peer border-blue-gray-200 border-t-transparent text-blue-gray-700 outline outline-0 placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-gray-900 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
-        placeholder=" " type='text' name='from_name' required />
-      <label
-        className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none !overflow-visible truncate text-[11px] font-normal leading-tight text-gray-500 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.1] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-gray-900 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:!border-gray-900 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:!border-gray-900 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
-        Name
-      </label>
-    </div>
-    <div className="-ml-2.5">
-      <div className="inline-flex items-center">
-        <label htmlFor="checkbox" className="relative flex items-center p-3 rounded-full cursor-pointer">
-         
-          <span
-            className="absolute text-white  transition-opacity opacity-0 pointer-events-none top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 peer-checked:opacity-100">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-3.5 w-3.5" viewBox="0 0 20 20" fill="currentColor"
-              stroke="currentColor" strokeWidth="1">
-              <path fillRule="evenodd"
-                d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                clipRule="evenodd"></path>
-            </svg>
-          </span>
-        </label>
-       
+            {/* */}
+            <div className="flex gap-16 items-center justify-center my-10">
+              <div>
+                <BiAtom className="text-3xl  text-success" />
+              </div>
+              <div>
+                <h1 className="text-2xl font-bold">Our Mission</h1>
+                <p>PetHub's mission is to build a thriving community of pet lovers. We provide a safe and reliable platform for pet adoption, fostering meaningful connections between pets and their new families.</p>
+              </div>
+            </div>
+            {/* */}
+            <div className="flex gap-16 items-center justify-center">
+              <div>
+                <BsFillBriefcaseFill className="text-3xl  text-red-600" />
+              </div>
+              <div>
+                <h1 className="text-2xl font-bold">Our Values</h1>
+                <p>PetHub is guided by compassion, integrity, and a deep respect for all animals. We believe in ethical pet adoption practices and strive to create a positive impact on the lives of pets and their owners.</p>
+              </div>
+            </div>
+          </div>
+          <div>
+            <img src={aboutus} alt="" className="w-9/12 rounded-lg hidden lg:flex" />
+          </div>
+        </div>
       </div>
     </div>
-    <div><textarea name='message' required  className="textarea textarea-bordered w-full" placeholder="Write Your Message"></textarea></div>
-  </div>
-  <div className="p-6 ">
-   <input type='submit' className='w-full bgg cursor-pointer text-white font-bold bg-[#FF4081]  rounded-full py-3' value='Send'/>
-    {/* <button
-      className="block w-full select-none rounded-lg bg-gradient-to-tr from-gray-900 to-gray-800 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-gray-900/10 transition-all hover:shadow-lg hover:shadow-gray-900/20 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-      type="button">
-      Send
-    </button>
-   </input>  */}
-  </div>
-  </form>
-</div>
-          </div>
-          <ToastContainer/>
-        </div>
-    );
+  );
 };
 
-export default Contact;
+export default AboutUs;
