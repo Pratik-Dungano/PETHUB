8827249ae68a9811be32c8c4032cc86253766ddf
Pratik <pratikpemendra@gmail.com>
2024-09-15 09:46:09 +0530

    few changes

--- Pet-Adoption-Platform-main/src/main.jsx
+++ Pet-Adoption-Platform-main/src/main.jsx
@@ -1,30 +1,32 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import { RouterProvider } from 'react-router-dom'
-import router from './components/router/Router'
-import AuthProvider from './components/providers/AuthProvider'
-
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { RouterProvider } from 'react-router-dom';
+import router from './components/router/Router';
+import AuthProvider from './components/providers/AuthProvider';
 import {
-  useQuery,
-  useMutation,
-  useQueryClient,
   QueryClient,
   QueryClientProvider,
-} from '@tanstack/react-query'
-// Create a client
-const queryClient = new QueryClient()
+} from '@tanstack/react-query';
+import { Auth0Provider } from '@auth0/auth0-react';
 
-
-
-
+// Create a client for react-query
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-  <AuthProvider>
- <QueryClientProvider client={queryClient}>
- <RouterProvider router={router} />
- </QueryClientProvider>
-  </AuthProvider>
- </React.StrictMode>,
-)
+    <Auth0Provider
+      domain="dev-bfalgnu6ukdtch07.us.auth0.com"
+      clientId="KpkqmDmNGw8AjscnkHX6Lt15YPHqHS60"
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+    >
+      <AuthProvider>
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+        </QueryClientProvider>
+      </AuthProvider>
+    </Auth0Provider>
+  </React.StrictMode>
+);
